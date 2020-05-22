24c8398e61e90b6ef031c25bd1745de0e6cc231b
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-05-22 12:35:19 -0400

    Initial commit

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        elevation: 5.0,
+        child: Icon(Icons.add, color: Colors.white,),
+        onPressed: (){
+
+        },
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(30.0, 60.0, 30.0, 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Icon(Icons.list, size: 30.0, color: Colors.lightBlueAccent),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(height:10.0),
+                Text('Todoey',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.w700
+                  ),
+                ),
+                Text('12 tasks',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0))
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
